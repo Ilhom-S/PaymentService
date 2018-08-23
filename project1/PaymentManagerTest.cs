a831d858a6e72f49373d156d258c868a98cf83ca
Ilkhom Safarov <ilhomjon.s@gmail.com>
2018-08-23 18:32:48 +0500

    Fixed java serializer

--- project1/PaymentManagerTest.cs
+++ project1/PaymentManagerTest.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace project1
+namespace PaymentService
 {
     class PaymentManagerTest
     {
--- project1/Properties/AssemblyInfo.cs
+++ project1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("project1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Payment Service")]
+[assembly: AssemblyDescription("Payment service for NativePay")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("project1")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Payment Service")]
+[assembly: AssemblyCopyright("Copyright ©  Mansur Saarov2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
